7bd3d2fc19bd048bb54c7a76110a740bf5d4bd9e
Xelot93 <xelot_93@hotmail.com>
2017-01-27 08:54:21 +0100

    aitor 2

---
+++ IMH_Desktop/gui/Newmachine2.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace IMH_Desktop.gui
+{
+    /// <summary>
+    /// Lógica de interacción para Newmachine2.xaml
+    /// </summary>
+    public partial class Newmachine2 : Window
+    {
+        public Newmachine2()
+        {
+            InitializeComponent();
+        }
+
+        private void addMachine_Click(object sender, RoutedEventArgs e)
+        {
+            Machine machine = new Machine();
+            machine.Code = textBoxCode.Text;
+            machine.SerialNumber = textBoxSerial.Text;
+            machine.Severity = textBoxSeverity.Text;
+            machine.WorkzoneId = comboBoxSection.SelectedItem.ToString()
+            machine.Type = comboBoxType.SelectedItem.ToString();
+
+        }
+    }
+}
